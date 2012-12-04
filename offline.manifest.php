7e70a42a29e3676edbe1a44085f32fd97e63d640
Elliott Foster <elliott@fourkitchens.com>
2012-12-04 11:07:42 -0600

    Fixed offline manifest.

--- offline.manifest.php
+++ offline.manifest.php
@@ -4,8 +4,8 @@ header("Content-Type: text/cache-manifest");
 CACHE MANIFEST
 <?php echo('#'. date(DATE_RSS)); ?>
 assets/js/libs/modernizr.min.js
-assets/js/libs/require.js
-assets/css/main.css
+dist/release/require.js
+dist/release/index.css
 /
 NETWORK:
 *
