896d3aae4eb30cea8f4c3eabbe96b70490eb7807
Elliott Foster <elliottf@codebrews.com>
2013-02-01 12:08:15 -0600

    Variable title.

--- app/example.conf.js
+++ app/example.conf.js
@@ -1,5 +1,10 @@
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 define([ ], function() {
   return {
+    // The title that will be used throughout the site.
+    title: 'You',
     // true if you want to include the 4k blog.
     blog: true,
     // Set the position in the menu for the blog.
